522f3e826bc811fce824c29b6340fea90666f2bb
xu_tao <121386234@qq.com>
2022-05-20 16:27:25 +0800

    first commit

---
+++ lib_config_app_util/src/main/java/com/example/lib_config_app_util/MyClass.java
@@ -0,0 +1,18 @@
+package com.example.lib_config_app_util;
+
+public class MyClass {
+
+    private String name;
+
+    public MyClass(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
